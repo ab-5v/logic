2dce5f041332d25d0bcc3e8be215f406106315ae
Artur Burtsev <artjock@gmail.com>
2013-05-08 22:13:25 +0400

    inital

---
+++ index.js
@@ -0,0 +1,170 @@
+var pzero = require('pzero');
+
+/**
+ * Logic constructor and getter
+ *
+ * @param {String} name
+ * @param {Object} params
+ * @param {Object} options
+ *
+ * @returns pzero
+ */
+function logic(name, params, options) {
+    params = params || {};
+    options = options || {};
+
+    if (!logic._list[name]) {
+        logic.error('Logic ' + name + ' not defined');
+    } else {
+        return Object.create(logic._list[name])._create(params, options);
+    }
+}
+
+
+logic.prototype = {
+
+    /**
+     * Creates logic instance and calls ready, when all deps are resolved
+     *
+     * @private
+     * @param {Object} params
+     * @param {Object} options
+     *
+     * @returns pzero
+     */
+    _create: function(params, options) {
+        var that = this;
+        return this._ensure(params, options)
+            .then(function(results) {
+                return that.ready(results, params, options);
+            });
+    },
+
+    /**
+     * Creates promises with all logics this one dependent on
+     *
+     * @private
+     *
+     * @param {Object} params
+     * @param {Object} options
+     *
+     * @returns pzero
+     */
+    _ensure: function(params, options) {
+        var promises = [];
+
+        var deps = this.deps || [];
+
+        deps.forEach(function(dep) {
+            var provider = logic._provider(dep);
+            if (provider) {
+                promises.push( provider(dep, params) );
+            } else {
+                logic.error('No provider found for ' + dep);
+            }
+        });
+
+        return pzero.when( promises );
+    },
+
+    /**
+     * Should be overwritten
+     * @param {Object} data
+     *
+     * @return Object
+     */
+    ready: function() {}
+};
+
+/**
+ * List of logic definitions, that can be created
+ *
+ * @private
+ * @type Object
+ */
+logic._list = {};
+
+/**
+ * List of logic data providers
+ * with default one by logic itself
+ *
+ * @private
+ * @type Object
+ */
+logic._providers = [
+    function(name) { return logic._list[name] && logic; }
+];
+
+/**
+ * Provider getter
+ *
+ * @static
+ * @private
+ *
+ * @param {String} name
+ * @param {Object} params
+ * @param {Object} options
+ *
+ * @return Function
+ */
+logic._provider = function(name, params, options) {
+    var provider;
+    var providers = logic._providers;
+
+    for (var i = 0; i < providers.length; i++ ) {
+        provider = providers[i](name, params, options);
+
+        if (typeof provider === 'function') {
+            return provider;
+        }
+    }
+};
+
+/**
+ * Provider setter
+ * External libs can set their own providers for logic.
+ * They should pass function, which can ensure,
+ * that lib can provide informations for given logic name
+ *
+ * This function will recive
+ *  - name
+ *  – params
+ *  – options
+ * to provide information
+ *
+ * @static
+ * @public
+ *
+ * @param {Function} checker
+ */
+logic.provider = function(func) {
+    logic._providers.push(func);
+};
+
+/**
+ * Defines logic, should provide name and dependencies.
+ * This logic will be stored and can be created and filled later
+ * with logic constructor
+ *
+ * @static
+ * @public
+ *
+ * @param {String} name
+ * @param {Object} extend
+ */
+logic.define = function define(name, extend) {
+    extend = extend || {};
+
+    var properties = {};
+    Object.getOwnPropertyNames(extend).forEach(function(name) {
+        properties[name] = Object.getOwnPropertyDescriptor(extend, name);
+    });
+
+    logic._list[name] = Object.create(logic.prototype, properties);
+};
+
+logic.error = function(msg) {
+    throw new Error(msg);
+};
+
+module.exports = logic;
---
+++ test/spec/logic.js
@@ -0,0 +1,126 @@
+/*jshint sub:true */
+
+var pzero = require('pzero');
+
+var sinon = require('sinon');
+var expect = require('expect.js');
+
+var logic = require('../../');
+
+describe('logic', function() {
+
+
+    beforeEach(function() {
+        logic._list = {};
+    });
+
+    describe('define', function() {
+
+        it('should save logic to a list', function() {
+            logic.define('test');
+
+            expect( logic._list['test'] ).to.be.an(Object);
+        });
+
+        it('should create logic with prototype', function() {
+            logic.define('test');
+
+            expect( Object.getPrototypeOf(logic._list['test']) ).to.eql(logic.prototype);
+        });
+
+        it('should extend logic', function() {
+            logic.define('test', {a: 1});
+
+            expect( logic._list['test'].a ).to.eql(1);
+        });
+
+        it('should not affect original object', function() {
+            logic.define('test');
+            logic._list['test']._create = 1;
+
+            expect( logic.prototype._create ).to.be.a( Function );
+        });
+    });
+
+    describe('constructor', function() {
+
+        it('should call error when no definition found', function() {
+            sinon.stub(logic, 'error');
+            logic('none');
+
+            expect( logic.error.called ).to.be.ok();
+
+            logic.error.restore();
+        });
+
+    });
+
+    describe('_create', function() {
+
+        it('should return promise', function() {
+            logic.define('test', {});
+            this.logic = logic._list['test'];
+
+            expect(pzero.is( this.logic._create() )).to.be.ok();
+        });
+
+    });
+
+    describe('_ensure', function() {
+
+        beforeEach(function() {
+            logic.define('test', {});
+
+            this.logic = logic._list['test'];
+
+            this.provider = function(to) {
+                return function() {
+                    var promise = pzero();
+                    setTimeout(function() { promise.resolve('res' + to); }, to);
+                    return promise;
+                };
+            };
+        });
+
+        it('should call error when no provider found', function() {
+            sinon.stub(logic, 'error');
+            this.logic.deps = ['123'];
+            this.logic._ensure();
+
+            expect(logic.error.called).to.be.ok();
+
+            logic.error.restore();
+        });
+
+        it('should return promise on no deps', function(done) {
+            this.logic._ensure()
+                .then(function(res) {
+                    expect( res ).to.eql( [] );
+                    done();
+                });
+        });
+
+        it('should wait for dep', function(done) {
+            logic.provider(this.provider);
+            this.logic.deps = ['10'];
+
+            this.logic._ensure()
+                .then(function(res) {
+                    expect( res ).to.eql( ['res10'] );
+                    done();
+                });
+        });
+
+        it('should wait for multiple deps', function(done) {
+            logic.provider(this.provider);
+            this.logic.deps = ['10', '20'];
+
+            this.logic._ensure()
+                .then(function(res) {
+                    expect( res ).to.eql( ['res10', 'res20'] );
+                    done();
+                });
+        });
+    });
+
+});
